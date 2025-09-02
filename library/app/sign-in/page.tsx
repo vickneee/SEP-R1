1b7f1a814ec48ba3747e4b76adcf682fb94a94dc
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-09-02 23:30:17 +0300

    Add sign in page

---
+++ library/app/sign-in/page.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/app/components/sections/footer/Footer";
+
+function SignIn() {
+    return (
+        <div className="font-sans items-center justify-items-center min-h-screen">
+            <main>
+                <h1 className="text-2xl font-bold">
+                    Sign In
+                </h1>
+            </main>
+            <Footer />
+        </div>
+    );
+}
+
+export default SignIn;
