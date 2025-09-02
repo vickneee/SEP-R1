84e01a073a0b6a7df1568c0feccbbf68360a1e2b
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-09-03 01:11:14 +0300

    Update Home page

--- library/app/page.tsx
+++ library/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div className="font-sans items-center justify-items-center min-h-screen">
             <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-                <h1 className="text-4xl sm:text-6xl font-extrabold text-center sm:text-left">
+                <h1 className="text-2xl font-bold text-center">
                     Welcome to LibraryHub </h1>
             </main>
             <Footer/>
