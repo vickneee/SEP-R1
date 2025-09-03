9f393b0e8a50da38b1c18285cf2a80952abecc95
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-09-03 21:43:44 +0300

    Fix text color

--- library/app/components/sections/hero/Hero.tsx
+++ library/app/components/sections/hero/Hero.tsx
@@ -18,12 +18,12 @@ function Hero() {
       </div>
         {/* Content */}
       <div className="relative z-10 text-center">
-        <h1 className="text-4xl md:text-6xl font-bold drop-shadow-md text-white">
+        <h1 className="text-4xl md:text-6xl font-bold drop-shadow-md text-gray-200">
             Discover Your Next
             <br />
             <span className="text-orange-600">Great Read </span>
         </h1>
-          <p className="text-white md:text-lg max-w-116 mx-auto mb-6 mt-2 drop-shadow-md">
+          <p className="text-gray-200 md:text-lg max-w-116 mx-auto mb-6 mt-2 drop-shadow-md">
               Browse thousands of books, reserve your favorites, and manage your reading journey all in one place.
           </p>
           {/* Search Bar */}
@@ -31,7 +31,7 @@ function Hero() {
               <input
                   type="text"
                   placeholder="Search by title, author, or ISBN..."
-                  className="w-full rounded-sm max-w-lg px-6 py-3 text-lg text-gray-800 placeholder-gray-500 bg-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition-colors duration-300"
+                  className="w-full rounded-sm max-w-lg px-6 py-3 text-lg text-gray-800 placeholder-gray-500 bg-gray-200 focus:outline-none focus:ring-2 focus:ring-orange-600 transition-colors duration-300"
               />
               <div className="absolute right-10 top-1.5 h-full ml-4 flex-shrink-0 hidden sm:block">
                   <Link href="/" passHref>
