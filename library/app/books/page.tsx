689699e63bedfff298452ef9d8833c54e4b189f6
Monami <monami.saeki@gmail.com>
2025-09-10 02:00:19 +0300

    Implement books page to fetch database data and add book detail page

--- library/app/books/page.tsx
+++ library/app/books/page.tsx
@@ -2,7 +2,6 @@ import { Books } from "@/app/components/custom/Books";
 import { loadSearchParams } from "@/app/components/custom/search-params";
 import type { SearchParams } from "nuqs/server";
 import { getBooksByTitle } from "@/app/actions/bookActions";
-import { getProducts } from "@/server/products";
 
 type PageProps = {
   searchParams: Promise<SearchParams>;
@@ -10,7 +9,6 @@ type PageProps = {
 
 export default async function BookPage({ searchParams }: PageProps) {
   const { search } = await loadSearchParams(searchParams);
-  //const { books } = await getBooksByTitle({search});
-  const books = await getProducts({ search });
+  const { books } = await getBooksByTitle(search);
   return <Books books={books} />;
 }
