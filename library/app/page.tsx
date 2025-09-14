fff81635346ed97a07da159c90c207090daa1e55
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-09-14 23:02:04 +0300

    Update main page to render AvailableBooks section

--- library/app/page.tsx
+++ library/app/page.tsx
@@ -1,14 +1,38 @@
-import Footer from "@/components/sections/Footer";
+"use client"
+
+import React, { useEffect, useState } from "react";
 import Hero from "@/components/sections/Hero";
 import AvailableBooks from "@/components/sections/AvailableBooks";
 import About from "@/components/sections/About";
+import Footer from "@/components/sections/Footer";
+
+import { createClient } from "@/utils/supabase/client";
 
 export default function Home() {
+    const [books, setBooks] = useState<any[]>([]);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const { data, error } = await createClient()
+                .from("books")
+                .select("*")
+
+            if (error) {
+                setError(error.message)
+            } else {
+                setBooks(data)
+            }
+        }
+
+        fetchBooks()
+    }, [])
+
     return (
         <div className="font-sans items-center justify-items-center min-h-screen">
             <main className="w-full">
                 <Hero/>
-                <AvailableBooks />
+                <AvailableBooks books={books} error={error} />
                 <About />
             </main>
             <Footer/>
