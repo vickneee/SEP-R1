3a497e1ad2da09fffa0a6c5a29d5b00f8762d0a1
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-09-20 17:34:27 +0300

    Add overdue function and visible label to UI

--- library/app/(dashboard)/customer-dashboard/UserReservations.tsx
+++ library/app/(dashboard)/customer-dashboard/UserReservations.tsx
@@ -90,8 +90,14 @@ export default function UserReservations() {
                 setIsReturning({...isReturning, [reservationId]: false});
                 setLoading(false);
             }
-        }
-    ;
+        };
+
+    function isOverdue(res: ReservationWithBook): boolean {
+        if (res.status !== "active") return false; // Only active ones can be overdue
+        const today = new Date();
+        const dueDate = new Date(res.due_date);
+        return dueDate < today;
+    }
 
     if (!reservations || reservations.length === 0) {
         return <p className="text-gray-600">You don’t have any borrowed books.</p>;
@@ -125,13 +131,14 @@ export default function UserReservations() {
                                 {res.return_date ? new Date(res.return_date).toISOString().slice(0, 10) : "-"}
                             </td>
                             <td className="px-4 py-2">
-                                    <span className={`px-2 py-1 rounded text-xs ${res.status === "active"
-                                        ? "bg-green-100 text-green-700"
-                                        : res.status === "returned"
-                                            ? "bg-blue-100 text-blue-700"
-                                            : "bg-red-100 text-red-700"
+                                    <span className={`px-2 py-1 rounded text-xs ${
+                                        res.status === "returned"
+                                        ? "bg-blue-100 text-blue-700"
+                                        : isOverdue(res)
+                                            ? "bg-red-100 text-red-700"
+                                            : "bg-green-100 text-green-700"
                                     }`}>
-                                        {res.status}
+                                        {res.status === "returned" ? "returned" : isOverdue(res) ? "overdue" : "active"}
                                     </span>
                             </td>
                             <td className="px-4 py-2 text-center">
