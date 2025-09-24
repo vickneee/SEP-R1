434af028b4ca44d1bbb072bb77a8339865b705a0
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-09-24 17:22:02 +0300

    Update params type

--- library/app/api/books/[bookId]/extend/route.ts
+++ library/app/api/books/[bookId]/extend/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { extendReservation } from "@/app/books/extendedAction";
 
-export async function POST(request: Request, { params }: { params: { reservationId: number } }) {
+export async function POST(request: Request, { params }: { params: { reservationId: string } }) {
     try {
         const updated = await extendReservation(params.reservationId);
         return NextResponse.json(updated, { status: 200 });
