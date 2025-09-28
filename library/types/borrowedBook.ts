42e6f2fdaf93a9a2b7253c5fd24c40dbbd9fcb90
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-09-28 18:18:27 +0300

    Add borrowed books types file

---
+++ library/types/borrowedBook.ts
@@ -0,0 +1,9 @@
+export type BorrowedBook = {
+    reservation_id: number;
+    user_name: string;
+    user_email: string;
+    book_title: string;
+    book_author: string;
+    due_date: string;
+    user_id: string;
+};
