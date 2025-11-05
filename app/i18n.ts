fdaf1583aa2348368897198ec3857f2bf294f3d0
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-11-05 12:42:58 +0200

    Update types

--- app/i18n.ts
+++ app/i18n.ts
@@ -1,27 +1,27 @@
-import { createInstance } from 'i18next';
+import {createInstance, Resource} from 'i18next';
 import { initReactI18next } from 'react-i18next/initReactI18next';
 import resourcesToBackend from 'i18next-resources-to-backend';
 import i18nConfig from '@/i18nConfig';
 
 export default async function initTranslations(
-  locale,
-  namespaces,
-  i18nInstance,
-  resources
+  locale: string,
+  namespaces: string[],
+  i18nInstance: ReturnType<typeof createInstance> | undefined,
+  resources: Resource | undefined
 ) {
   i18nInstance = i18nInstance || createInstance();
-  
+
   i18nInstance.use(initReactI18next);
-  
+
   if (!resources) {
     i18nInstance.use(
       resourcesToBackend(
-        (language, namespace) =>
+        (language: string, namespace: string) =>
           import(`@/locales/${language}/${namespace}.json`)
       )
     );
   }
-  
+
   await i18nInstance.init({
     lng: locale,
     resources,
@@ -32,7 +32,7 @@ export default async function initTranslations(
     ns: namespaces,
     preload: resources ? [] : i18nConfig.locales
   });
-  
+
   return {
     i18n: i18nInstance,
     resources: { [locale]: i18nInstance.services.resourceStore.data[locale] },
