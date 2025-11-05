a2720c53df7bf93d8767707f7f7bbceca911e328
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-11-05 13:26:21 +0200

    Remove unused variable

--- app/i18n.js
+++ app/i18n.js
@@ -1,4 +1,4 @@
-import {createInstance, Resource} from 'i18next';
+import {createInstance} from 'i18next';
 import { initReactI18next } from 'react-i18next/initReactI18next';
 import resourcesToBackend from 'i18next-resources-to-backend';
 import i18nConfig from '@/i18nConfig';
