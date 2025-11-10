2c2b237b8bbd08438b8fb61a018c13311722ef1f
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-11-10 02:11:30 +0200

    Update params private page

--- app/[locale]/private/page.tsx
+++ app/[locale]/private/page.tsx
@@ -2,15 +2,16 @@ import {getUserProfile} from "./userProfile-action";
 import initTranslations from "@/app/i18n";
 import {redirect} from "next/navigation";
 
-type PrivatePageProps = {
-    params: { locale: string };
-};
+interface Params {
+    locale?: string;
+}
 
-export default async function PrivatePage({params}: PrivatePageProps) {
-    const routeLocale = params?.locale ?? "en";
+export default async function PrivatePage({params}: { params: Promise<Params> }) {
+    const { locale: routeLocale = "en" } = await params;
 
     const userProfile = await getUserProfile();
-    const locale = userProfile?.language || routeLocale || "en";
+
+    const locale = userProfile?.language ?? routeLocale ?? "en";
 
     const {t} = await initTranslations(locale, ["private"]);
 
@@ -33,7 +34,6 @@ export default async function PrivatePage({params}: PrivatePageProps) {
     }
 
     return (
-
         <div className="max-w-2xl mx-auto p-6">
             <h1 className="text-2xl font-bold mb-4">
                 {t("private_error_access_denied")}
