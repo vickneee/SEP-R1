9f214cdc9c11e92b87e047a4c77da5b88b94a60a
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-11-14 14:18:35 +0200

    Refactor code update code

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -129,8 +129,11 @@ function Carousel({
 
     return (
         <CarouselContext.Provider value={contextValue}>
-            <div onKeyDownCapture={handleKeyDown} className={cn("relative", className)} role="region"
-                 aria-roledescription="carousel" data-slot="carousel"
+            <div onKeyDownCapture={handleKeyDown}
+                 className={cn("relative", className)}
+                 aria-label="Carousel"
+                 aria-roledescription="carousel"
+                 data-slot="carousel"
                  {...props}
             >
                 {children}
@@ -159,11 +162,12 @@ function CarouselItem({className, ...props}: React.ComponentProps<"div">) {
     const {orientation} = useCarousel()
 
     return (
-        <div role="group" aria-roledescription="slide" data-slot="carousel-item" className={cn(
+        <div data-slot="carousel-item" className={cn(
             "min-w-0 shrink-0 grow-0 basis-full",
             orientation === "horizontal" ? "pl-4" : "pt-4",
             className
         )}
+             aria-roledescription="slide"
              {...props}
         />
     )
