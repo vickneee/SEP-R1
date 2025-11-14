1404620775a937c28be55824dce94b8690af9b91
Monami <monami.saeki@gmail.com>
2025-11-15 00:21:25 +0200

    changed code back to the original

--- components/schemas/updateEmailSchema.ts
+++ components/schemas/updateEmailSchema.ts
@@ -5,7 +5,5 @@ export const getUpdateEmailSchema = (t: (key: string) => string) =>
     email: z
       .string()
       .min(1, { message: t("dashboard_validation_email_required") })
-      .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, {
-        message: t("dashboard_validation_email_invalid"),
-      }),
+      .email({ message: t("dashboard_validation_email_invalid") }),
   });
